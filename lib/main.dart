a8ecfd27a3096b87d5fe3c1205855ae5deadfabf
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-25 13:11:28 +0530

    Basic Understanding of flutter showing hello world!

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//Types of Widgets
+// 1. StatelessWidgets: the data is immutable. That is once the widget is create it cannot be changed
+// 2. StatefulWidget: the data is mutable.
+// 3. InheritedWidget
+
+// State: it refers to data how widget cares about.
+//key : it is a class that helps flutter identify and differenciate between classes
+//My Custom Widget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello World', textDirection: TextDirection.ltr);
+  }
+}
