3b0aa0cef309e6db1bf4e47463768704c8ea2b23
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-25 14:21:06 +0530

    Adding a column and centering it horizontally

--- lib/currency_converter_material_design.dart
+++ lib/currency_converter_material_design.dart
@@ -4,6 +4,14 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Hello World!")));
+    return Scaffold(body: Center(
+      child: 
+      Column(
+      children: [
+        Text("0")
+        ]
+        ),
+    )
+        );
   }
 }
