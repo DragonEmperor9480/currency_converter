fd2cf1f193c86d86f8943e371a5c23d6dfe3f606
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-26 08:18:52 +0530

    Playing with colors in text and scaffold

--- lib/currency_converter_material_design.dart
+++ lib/currency_converter_material_design.dart
@@ -4,11 +4,18 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      Text("0")
+      Text("0",
+      style: TextStyle(
+        fontSize: 50,
+        fontWeight: FontWeight.normal,
+        color: Color.fromARGB(255, 255, 255, 255)
+        ),)
       ]
       ),
     )
