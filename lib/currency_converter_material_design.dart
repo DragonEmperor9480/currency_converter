06fe43c0638913c8824bb0f12fc5e979c8e9c844
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-27 07:33:29 +0530

    create a custom method for the border, also use enableBorder.
    enableBorder enables the border properties by default unlike focused border

--- lib/currency_converter_material_design.dart
+++ lib/currency_converter_material_design.dart
@@ -4,45 +4,45 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Text("0",
-      style: TextStyle(
-        fontSize: 50,
-        fontWeight: FontWeight.normal,
-        color: Color.fromARGB(255, 255, 255, 255)
-        ),
-        ),
-        TextField(
-          style: TextStyle(
-            color: Colors.cyan,
-          ),
-          decoration: InputDecoration(
-            hintText: "Please Enter the amount in USD",
-            hintStyle: TextStyle(
-              color: Colors.green
-            ),
-            prefixIcon: Icon(Icons.attach_money_outlined),
-            prefixIconColor: Colors.red,
-            fillColor: Colors.white,
-            filled: true,
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.red,
-                width: 4.0,
-                style: BorderStyle.solid
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(30),)
-            ),
-          ),
-        )
-      ]
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.red,
+        width: 4.0,
+        style: BorderStyle.solid,
       ),
-    )
-        );
+      borderRadius: BorderRadius.all(Radius.circular(30)),
+    );
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.normal,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.cyan),
+              decoration: InputDecoration(
+                hintText: "Please Enter the amount in USD",
+                hintStyle: TextStyle(color: Colors.green),
+                prefixIcon: Icon(Icons.attach_money_outlined),
+                prefixIconColor: Colors.red,
+                fillColor: Colors.white,
+                filled: true,
+                focusedBorder: border,
+                enabledBorder: border,
+               
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
