9dfb556abaf7b2354db1437f9fd8020df2a4f3d5
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-27 12:44:15 +0530

    wrap Text in container and demonstrating the exampe of padding and margin

--- lib/currency_converter_material_design.dart
+++ lib/currency_converter_material_design.dart
@@ -18,12 +18,17 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "0",
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.normal,
-                color: Color.fromARGB(255, 255, 255, 255),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              color: Colors.black54,
+              child: Text(
+                "0",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.normal,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
               ),
             ),
             //margin: it deals with the stuff outside the container.
